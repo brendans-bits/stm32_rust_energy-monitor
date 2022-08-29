1662a5778760757daf7f0d0c2097607fb7d1fcf3
brendan <brendan@brendansbits.com>
2022-08-29 18:42:06 +1000

    Added serial output on pin A9

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,17 @@
 #![no_main]
 #![no_std]
 
+
 use stm32_rust_energy_monitor as _; // global logger + panicking-behavior + memory layout
 
 use stm32f4xx_hal as hal;
 
-use crate::hal::{prelude::*};
+use crate::hal::{
+    prelude::*,
+    serial::{Config, Serial},
+};
+
+use core::fmt::Write;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -26,12 +32,32 @@ fn main() -> ! {
     // Create a delay abstraction based on SysTick
     let mut delay = cp.SYST.delay(&clocks);
 
+    // Set up serial tx pin
+    let gpioa = dp.GPIOA.split();
+    let tx_pin = gpioa.pa9.into_alternate();
+
+    //create an object for serial transmission
+    let mut tx = Serial::tx(
+        dp.USART1,
+        tx_pin,
+        Config::default()
+            .baudrate(115200.bps())
+            .wordlength_8()
+            .parity_none(),
+            &clocks,
+        )
+      .unwrap();
+
+    //Write a line to serial output
+    writeln!(&mut tx, "Starting \r").unwrap();
+
     loop {
         // On for 0.2s, off for 1s.
         led.set_high();
         delay.delay_ms(1000_u32);
         led.set_low();
         delay.delay_ms(200_u32);
+        writeln!(&mut tx, "Blink \r").unwrap();
     }
 
 }
