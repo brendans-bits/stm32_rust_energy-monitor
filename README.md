5f35d9f0961b3fc61639ec92a62f8bebac87075b
brendan <brendan@brendansbits.com>
2022-10-28 20:39:37 +1100

    Minimal working code

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 This repository will host the code for running an mains energy monitor on the STM32 platform, written in Rust.
 
-As of August 2020:
+As of October 2022:
 - this code is written for the WeAct Black Pill (v2) which contains the STM32F411CEU6 microcontroller, but should be easily adaptable for other similar microcontrollers, and
-- it currently only contains a 'hello world' code example to get up and running.
+- it contains minimally working code that is calibrated for my physical set up.
 
 ## Acknowledgements
 
-Inspiration for the code to work out energy comes from [EmonLib](https://github.com/openenergymonitor/EmonLib).
+Inspiration for the code to work out power flow comes from [EmonLib](https://github.com/openenergymonitor/EmonLib).
+There are three slight differences to the original EmonLib:
+- The code provides an inital preseeded starting point for the DC offset which is removed from the raw ADC values that are obtained. This starting point assumes that the DC offset is VCC/2 and the ADC has 12 bits of resolution. The original EmonLib uses a digital low-pass filter formula to slowly increase the offset from 0. This allows the early 'filtered' values for voltage and current value to be roughly correct instead of having to wait until the low-pass filter formula settles.
+- The main measurement loop runs for a set amount of time (nominally 500ms) instead of running for a set number of wavelengths. This removes a set of calculations from the code.
+- There is no correction for any phase error coming from the voltage transformer. The toroidal transformer I am using produced negligible phase error.
 
 The code is written on top of the [app-template](https://github.com/knurling-rs/app-template) developed by [Knurling-rs](https://github.com/knurling-rs) and makes use of pieces of example code for the STM32 platform in the [STM32F4xx-hal crate](https://github.com/stm32-rs/stm32f4xx-hal).
 
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -29,21 +29,25 @@ fn main() -> ! {
     //---------------------------------------
     // Calibration Constants
     //---------------------------------------
-    let mut voltage_phase_calibration_value: f64 = -0.20; // Depends on which transformer I am using
-    let mut voltage_calibration_value: f64 = 257.0; // rough value only!
-    let mut voltage_correction_ratio: f64;
-    let mut current_calibration_value: f64 = 19.0; // rough value only!
-    let mut current_correction_ratio: f64;
+    // I have removed the need to apply a phase calibration for two reasons
+    // 1 - the toroidal transformer I am using has so little phase error that I don't seem to gain anything from applying a correction.
+    // 2 - I don't fully understand the equation provided in emonlib
+    // NOTE: If you want to use the phase correction, you will need to change some of the variable types
+    // i.e. change some variables from i128 to f64
+    //let voltage_phase_calibration_value: f64 = 0.00; // Depends on which transformer I am using
+    let voltage_calibration_value: f64 = 264.4; // rough value only!
+    let current_calibration_value: f64 = 18.45; // rough value only!
 
-    //
+    //----------------------------------------
     // Variable definitions
-    //
+    //----------------------------------------
 
     // Count how many times the voltage waveform has crossed zero
-    let mut zero_crossings_count: u32 = 0;
+    //let mut zero_crossings_count: u32 = 0;
     // Number of times we want the voltage waveform to cross zero
-    let zero_crossings_target: u32 = 100;
-    let mut samples_count: u32;
+    //let zero_crossings_target: u32 = 100;
+
+    let mut samples_count: i128;
 
     // Set timeout duration
     let timeout_duration: Duration<u32, 1, 1000000> = ExtU32::millis(2001);
@@ -57,8 +61,9 @@ fn main() -> ! {
     let mut voltage_sample_squared: i128;
     let mut voltage_sample_sum: i128;
     let mut voltage_rms: f64;
-    let mut voltage_phase_corrected: f64;
-    let mut voltage_prior_sample_offset_removed: i128;
+    //let mut voltage_phase_corrected: f64;
+    //let mut voltage_prior_sample_offset_removed: i128;
+    let mut voltage_correction_ratio: f64;
 
     let mut current_sample_measurement: u16;
     let mut current_adc_offset: i128;
@@ -66,17 +71,17 @@ fn main() -> ! {
     let mut current_sample_squared: i128;
     let mut current_sample_sum: i128;
     let mut current_rms: f64;
+    let mut current_correction_ratio: f64;
 
-
-    let mut power_sample_instantaneous: f64;
-    let mut power_sample_sum: f64;
-
+    let mut power_sample_instantaneous: i128;
+    let mut power_sample_sum: i128;
     let mut power_real: f64;
     let mut power_apparent: f64;
     let mut power_factor: f64;
-    //
+
+    //----------------------------------
     // Set up peripherals
-    //
+    //----------------------------------
 
     let dp = stm32f4xx_hal::pac::Peripherals::take().unwrap();
     let cp = cortex_m::peripheral::Peripherals::take().unwrap();
@@ -101,7 +106,7 @@ fn main() -> ! {
     //    dp.USART1,
     //    tx_pin,
     //    Config::default()
-    //        .baudrate(115200.bps())
+    //        .baudrate(1152000.bps())
     //        .wordlength_8()
     //        .parity_none(),
     //    &clocks,
@@ -111,11 +116,10 @@ fn main() -> ! {
     // Create a timer that we use to measuer whether our measurements have exceeded allowable time
     let timer = dp.TIM5;
     let mut timeout_timer = timer.counter_us(&clocks);
-    
 
     // Write a line to serial output
     // writeln!(&mut tx, "Starting \r").unwrap();
-    
+
     //writeln!(&mut tx, "counter, current_sample_offset_removed, voltage_sample_offset_removed, voltage_phase_corrected \r").unwrap();
 
     // Set up pin for voltage and current measurements
@@ -145,7 +149,10 @@ fn main() -> ! {
 
         // Pseudocode derived from emonlib
 
+        //-----------------------------------------
         // 1. Wait for the voltage sine wave to be roughly mid-cycle
+        //------------------------------------------
+
         // We will measure voltage because this will always give a measureable sine wave even if there is little current flowing.
 
         // 1a. Start a timer
@@ -176,86 +183,118 @@ fn main() -> ! {
             }
         }
 
+        //-------------------------------------
         // 2. Main measurement loop
-        // 2a. Start a timer
-        timeout_timer.start(timeout_duration).unwrap();
-        defmt::println!("Started timer...");
+        //-------------------------------------
 
-        voltage_adc_offset = 1784;
+        // 2a. Reset some variables used in the loop
+        voltage_adc_offset = 2048; // This assumes that you are adding a DC offset that is VCC/2 and a 12 bit ADC
         current_adc_offset = 2048;
         voltage_sample_sum = 0;
         current_sample_sum = 0;
-        power_sample_sum = 0.0;
+        power_sample_sum = 0;
         samples_count = 0;
-        voltage_prior_sample_offset_removed = 0;
-        // 2b. Start a loop that runs until the voltage waveform that we are measuring has crossed zero a set number of times OR a timeout has been reached
-        while (timeout_timer.now().duration_since_epoch()
-            < timeout_duration - fugit::ExtU32::millis(1))
+        //voltage_prior_sample_offset_removed = 0;
+
+        // 2b. Start a timer
+        timeout_timer.start(timeout_duration).unwrap();
+        //defmt::println!("Started timer...");
+
+        // 2c. Start a loop that runs until a timeout has been reached
+        while timeout_timer.now().duration_since_epoch()
+            < timeout_duration - fugit::ExtU32::millis(1500)
         {
             //let counter = timeout_timer.now().duration_since_epoch().ticks();
             //defmt::println!("timer count {:?}", counter );
             //let counter = timer.counter_us(&clocks).now().duration_since_epoch().ticks();
-            
+
             // Read raw ADC values for voltage and current
             voltage_sample_measurement = adc.convert(&pin_voltage, SampleTime::Cycles_3);
             current_sample_measurement = adc.convert(&pin_current, SampleTime::Cycles_3);
-            // Calculate DC offset
+
+            // Adjust DC offset
+            // Unlike emonlib, we are pre-seeding a value here
             voltage_adc_offset = voltage_adc_offset
                 + ((voltage_sample_measurement as i128 - voltage_adc_offset) / 1024);
             current_adc_offset = current_adc_offset
                 + ((current_sample_measurement as i128 - current_adc_offset) / 1024);
+
             // Remove DC offset from voltage and current values
             voltage_sample_offset_removed = voltage_sample_measurement as i128 - voltage_adc_offset;
             current_sample_offset_removed = current_sample_measurement as i128 - current_adc_offset;
+
             // Square values for voltage and current
             voltage_sample_squared = voltage_sample_offset_removed * voltage_sample_offset_removed;
             current_sample_squared = current_sample_offset_removed * current_sample_offset_removed;
+
             // Sum values for voltage and current
             voltage_sample_sum = voltage_sample_sum + voltage_sample_squared;
             current_sample_sum = current_sample_sum + current_sample_squared;
+
             // Apply phase calibration to voltage (to allow for phase delay in voltage transformer)
-            voltage_phase_corrected = voltage_prior_sample_offset_removed as f64
-                + voltage_phase_calibration_value
-                    * (voltage_sample_offset_removed - voltage_prior_sample_offset_removed) as f64;
+            //voltage_phase_corrected = voltage_prior_sample_offset_removed as f64
+            //    + voltage_phase_calibration_value
+            //        * (voltage_sample_offset_removed - voltage_prior_sample_offset_removed) as f64;
+            // See earlier comments about how I have removed the phase calibration because I don't seem to need it.
+            // If it goes back in, you will need to change some variables from i128 to f64
+
             // Calculate instantaneous power
-            power_sample_instantaneous = voltage_phase_corrected * current_sample_offset_removed as f64;
+            //power_sample_instantaneous = voltage_phase_corrected * current_sample_offset_removed as f64;
+            power_sample_instantaneous =
+                voltage_sample_offset_removed * current_sample_offset_removed;
+            //The toroid transformer has negligible phase delay.....
+
             // Sum of instantaneous power
             power_sample_sum = power_sample_sum + power_sample_instantaneous;
+
             // Count the number of samples we have taken
             samples_count = samples_count + 1;
+
             // Store previous offset-corrected voltage for use in the next iteration of the loop
-            voltage_prior_sample_offset_removed = voltage_sample_offset_removed;
+            //voltage_prior_sample_offset_removed = voltage_sample_offset_removed;
+
             // update a counter if the voltage sine wave has crossed zero
             // TO DO
+
             //writeln!(tx, "{}, {}, {}, {} \r", counter, current_sample_offset_removed, voltage_sample_offset_removed, voltage_phase_corrected).unwrap();
-            delay.delay_ms(1_u32); // I needed to add thsi delay in order for the phase compensation calculations to be correct
+            //writeln!(tx, "{}, {}, {} \r", counter, current_sample_offset_removed, voltage_sample_offset_removed).unwrap();
+
+            //delay.delay_ms(1_u32); // I needed to add thsi delay in order for the phase compensation calculations to be correct
             // Too many samples meant that the phase compensation was incorrect, giving me low real power values
             // Alternatively, I may need a 'better' voltage transformer...
         }
-        defmt::println!("voltage_sample_sum {} \r", voltage_sample_sum);
-        defmt::println!("current_sample_sum {} \r", current_sample_sum);
-        defmt::println!("power_sample_sum {} \r", power_sample_sum);
-        defmt::println!("samples_count {} \r", samples_count); 
-        // 3. Post loop
+        //defmt::println!("voltage_sample_sum {} \r", voltage_sample_sum);
+        //defmt::println!("current_sample_sum {} \r", current_sample_sum);
+        //defmt::println!("power_sample_sum {} \r", power_sample_sum);
+        defmt::println!("samples_count {} \r", samples_count);
+
+        //-----------------------
+        // 3. Post loop calculations
+        //-----------------------
+
         // 3a. Calculate voltage and current ratio values from calibration constants
         // Assumes that the device is operated from 3.3v
         voltage_correction_ratio = voltage_calibration_value * ((3300.0 / 1000.0) / 4096.0);
         current_correction_ratio = current_calibration_value * ((3300.0 / 1000.0) / 4096.0);
-        defmt::println!("voltage_correction_ratio {} \r", voltage_correction_ratio);
-        defmt::println!("current_correction_ratio {} \r", current_correction_ratio);        
+        //defmt::println!("voltage_correction_ratio {} \r", voltage_correction_ratio);
+        //defmt::println!("current_correction_ratio {} \r", current_correction_ratio);
+
         // 3b. Calculate RMS values for voltage and current
-        // NEED TO MULTIPLE THESE BY THE VOLTAGE AND CURRENT RATIO VALUES
         //voltage_rms = sqrt(voltage_sample_sum as f64 / samples_count as f64);
-        voltage_rms = voltage_correction_ratio * sqrt(voltage_sample_sum as f64 / samples_count as f64);
+        voltage_rms =
+            voltage_correction_ratio * sqrt(voltage_sample_sum as f64 / samples_count as f64);
         //current_rms = sqrt(current_sample_sum as f64 / samples_count as f64);
-        current_rms = current_correction_ratio * sqrt(current_sample_sum as f64 / samples_count as f64);
+        current_rms =
+            current_correction_ratio * sqrt(current_sample_sum as f64 / samples_count as f64);
         defmt::println!("RMS Voltage {} \r", voltage_rms);
         defmt::println!("RMS Current {} \r", current_rms);
+
         // 3c. Calculate real power, apparent power, power factor
-        //NEED TO MULTIPLY THIS BY VOLTAGE AND CURRENT CALIBRATION VALUES
         //power_real = power_sample_sum as f64 / samples_count as f64;
-        power_real = voltage_correction_ratio * current_correction_ratio * power_sample_sum as f64 / samples_count as f64;
-        power_apparent = voltage_rms as f64 * current_rms as f64;
+        power_real = voltage_correction_ratio
+            * current_correction_ratio
+            * (power_sample_sum / samples_count) as f64;
+        power_apparent = voltage_rms * current_rms;
         power_factor = power_real / power_apparent;
         defmt::println!("real power {} \r", power_real);
         defmt::println!("apparent power {} \r", power_apparent);
